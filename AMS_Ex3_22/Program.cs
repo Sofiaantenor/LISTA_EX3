bdf1106cbe3f9101b9526de113953e4479478da2
Sofiaantenor <90793659+Sofiaantenor@users.noreply.github.com>
2022-06-10 15:36:21 -0300

    Add files via upload

---
+++ AMS_Ex3_22/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace AMS_Ex3_22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int valorVendido = 500000;
+            int porcentagem = 5;
+            int resultado = valorVendido * porcentagem /100;
+
+            Console.WriteLine("");
+            Console.WriteLine($"O 5% de 500000 é: {resultado}");
+            Console.WriteLine("");
+        }
+    }
+}
